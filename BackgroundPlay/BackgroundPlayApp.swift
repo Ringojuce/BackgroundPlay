5646a489feafde848103c2b319fc9bb4f2d465bf
Ringojuce <sigo7100@gmail.com>
2024-07-29 21:45:19 +0900

    Initial Commit

---
+++ BackgroundPlay/BackgroundPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundPlayApp.swift
+//  BackgroundPlay
+//
+//  Created by miya on 2024/07/29.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
