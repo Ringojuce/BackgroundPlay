d7964d6ae766e0086b24fe692c789ff0e97be0e9
Ringojuce <sigo7100@gmail.com>
2024-08-08 00:01:55 +0900

    ひとまずコミット

--- BackgroundPlay/BackgroundPlayApp.swift
+++ BackgroundPlay/BackgroundPlayApp.swift
@@ -25,7 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         // オーディオセッションを設定
         do {
-//            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [])
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.mixWithOthers])
             try AVAudioSession.sharedInstance().setActive(true)
         } catch {
