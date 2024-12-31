4e759e40e0b1f4ca99bca41bb7acacf3db6ee1b3
Ringojuce <sigo7100@gmail.com>
2024-12-31 23:06:36 +0900

    再生ファイル一覧画面、オーディオコントロール画面追加

---
+++ BackgroundPlay/ContentListView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentListView.swift
+//  BackgroundPlay
+//
+//  Created by miya on 2024/12/31.
+//
+
+import SwiftUI
+
+struct ContentListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentListView()
+}
---
+++ ContentAudioControl.swift
@@ -0,0 +1,18 @@
+//
+//  ContentAudioControl.swift
+//  BackgroundPlay
+//
+//  Created by miya on 2024/12/31.
+//
+
+import SwiftUI
+
+struct ContentAudioControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentAudioControl()
+}
